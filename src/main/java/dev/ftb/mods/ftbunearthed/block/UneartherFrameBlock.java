c895243e39e5f793956cbf471aad23fc39bfb255
Des Herriott <des.herriott@gmail.com>
2025-09-23 14:28:17 +0100

    fix: unearther frame blocks are not pathfindable

--- src/main/java/dev/ftb/mods/ftbunearthed/block/UneartherFrameBlock.java
+++ src/main/java/dev/ftb/mods/ftbunearthed/block/UneartherFrameBlock.java
@@ -14,6 +14,7 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -117,6 +118,11 @@ public class UneartherFrameBlock extends Block implements EntityBlock {
         }
 
         return InteractionResult.sidedSuccess(level.isClientSide);
+    }
+
+    @Override
+    protected boolean isPathfindable(BlockState state, PathComputationType pathComputationType) {
+        return false;
     }
 
     public enum Part {
