f871a84e9e2474c6f89d5d5eae0ed0620a49f69e
Des Herriott <des.herriott@gmail.com>
2025-10-08 12:09:28 +0100

    chore: update to KJS 7.2 (breaking changes)

--- src/main/java/dev/ftb/mods/ftbunearthed/integration/kubejs/ItemWithChanceComponent.java
+++ src/main/java/dev/ftb/mods/ftbunearthed/integration/kubejs/ItemWithChanceComponent.java
@@ -1,12 +1,23 @@
 package dev.ftb.mods.ftbunearthed.integration.kubejs;
 
 import com.mojang.serialization.Codec;
+import dev.ftb.mods.ftbunearthed.FTBUnearthed;
 import dev.ftb.mods.ftbunearthed.crafting.ItemWithChance;
 import dev.latvian.mods.kubejs.recipe.component.RecipeComponent;
+import dev.latvian.mods.kubejs.recipe.component.RecipeComponentType;
 import dev.latvian.mods.rhino.type.TypeInfo;
 
-public class ItemWithChanceComponent implements RecipeComponent<ItemWithChance> {
-    public static final ItemWithChanceComponent INSTANCE = new ItemWithChanceComponent();
+public enum ItemWithChanceComponent implements RecipeComponent<ItemWithChance> {
+    INSTANCE;
+
+    private static final String ID_STR = "item_with_chance";
+    private static final RecipeComponentType.Unit<ItemWithChance> TYPE
+            = RecipeComponentType.unit(FTBUnearthed.id(ID_STR), INSTANCE);
+
+    @Override
+    public RecipeComponentType<?> type() {
+        return TYPE;
+    }
 
     @Override
     public Codec<ItemWithChance> codec() {
@@ -20,6 +31,6 @@ public class ItemWithChanceComponent implements RecipeComponent<ItemWithChance> 
 
     @Override
     public String toString() {
-        return "item_with_chance";
+        return ID_STR;
     }
 }
